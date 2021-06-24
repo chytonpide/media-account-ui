da2a01332ab2271c18883b8afd3398808341e963
username <yadameda@naver.com>
2021-06-24 19:57:49 +0900

    commit aditional files

---
+++ src/components/common/ModalSpinner.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Modal, Button } from "react-bootstrap";
+import { threadId } from "worker_threads";
+import "./ModalSpinner.css";
+
+export interface ModalSpinnerProps {
+  show: boolean;
+}
+
+export default class ModalSpinner extends React.Component<ModalSpinnerProps> {
+  private modalElement: React.RefObject<HTMLDivElement>;
+
+  constructor(props: ModalSpinnerProps) {
+    super(props);
+    this.modalElement = React.createRef();
+  }
+
+  public render() {
+    let style = "modal-spinner-container";
+    if (this.props.show === false) {
+      const el = this.modalElement.current;
+      if (el != null) {
+        setTimeout(() => (el.style.display = "none"), 350);
+      }
+    }
+
+    /*
+    if (this.props.show === false) {
+      style = style + " not-showing";
+    }*/
+    /*style={{ opacity: this.props.show ? "1" : "0" }}*/
+    return (
+      <div
+        ref={this.modalElement}
+        className="modal-spinner-container"
+        style={{ opacity: this.props.show ? "1" : "0" }}
+      >
+        <div className="modal-spinner-spinner"></div>
+      </div>
+    );
+  }
+}
