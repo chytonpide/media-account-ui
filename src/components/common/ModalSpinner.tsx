9de7c12282cb99c9faa96cf6a4695c279108bdcc
username <yadameda@naver.com>
2021-10-18 14:26:08 +0900

    modal spinner changed.

--- src/components/common/ModalSpinner.tsx
+++ src/components/common/ModalSpinner.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { Modal, Button } from "react-bootstrap";
-import { threadId } from "worker_threads";
 import "./ModalSpinner.css";
 
 export interface ModalSpinnerProps {
--- src/properties.js
+++ src/properties.js
@@ -1,3 +1,4 @@
 export const properties = {
-  apiOrigin: "http://133.18.48.25:8080/automan",
+  /*apiOrigin: "http://133.18.48.25:8080/automan",*/
+  apiOrigin: "http://localhost:9095/automan",
 };
